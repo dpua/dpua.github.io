af76ead410ee5c0f670bf4fba8b44a280e4a2a42
dp.ua <dpua@mail.com>
2019-07-21 18:57:20 +0300

    Update telegramform.js

--- telegramform.js
+++ telegramform.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    alert(1);
     $(".form-element").submit(function () {
         var formID = $(this).attr('id');
         var formNm = $('#' + formID);
@@ -6,7 +7,7 @@ $(document).ready(function () {
         var formTitle = $(formNm).find(".form-title");
         $.ajax({
             type: "POST",
-            url: 'send-message-to-telegram.php',
+            url: 'https://api.telegram.org/bot876530454:AAHpvXr4X5hHTeq2L2g0qrRwRz4hx4_Q-jY/sendMessage?chat_id=-362609574&parse_mode=html&text='+formNm.serialize(),
             data: formNm.serialize(),
             success: function (data) {
               // Вывод сообщения об успешной отправке
