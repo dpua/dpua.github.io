c1ac2fa15a6c27e5143674bd3ac18ed307d3076a
dp.ua <dp.ua@mail.com>
2020-10-02 16:45:43 +0300

    Add files via upload

---
+++ reactapp/build/precache-manifest.52641a43e710da2a71f4b2d3d1a464e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef8508fd55e44c4e4817ffb16992ada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e010fd941a9c2174bd1",
+    "url": "/static/css/main.d6ac853a.chunk.css"
+  },
+  {
+    "revision": "2c86e76aed6896e9cff1",
+    "url": "/static/js/2.0c66ad19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0c66ad19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e010fd941a9c2174bd1",
+    "url": "/static/js/main.b35c7093.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "57f271079a80be19b72354d9eab06d08",
+    "url": "/static/media/img.57f27107.jpg"
+  },
+  {
+    "revision": "b4d33c1127f2a4b4626ee3b8ee6110cb",
+    "url": "/static/media/logo.b4d33c11.jpg"
+  }
+]);
