bd6ab58944e5694877f3aa14fb7b48c19f47e537
23dpua <23dpua@gmail.com>
2020-12-28 12:09:21 +0200

    add react app "Cake shop"

---
+++ cake/precache-manifest.f299d37294bc448718fcf9dbcb3fc104.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daaecd251aca36ecb1c03e6603a86e83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7f2b9f51ad29a5eb93c",
+    "url": "/static/css/main.abeaf44c.chunk.css"
+  },
+  {
+    "revision": "2c86e76aed6896e9cff1",
+    "url": "/static/js/2.0c66ad19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0c66ad19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f2b9f51ad29a5eb93c",
+    "url": "/static/js/main.64efd609.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "57f271079a80be19b72354d9eab06d08",
+    "url": "./static/media/img.57f27107.jpg"
+  },
+  {
+    "revision": "840aa75b57fe6191c6c719ca3a33436f",
+    "url": "./static/media/logo.840aa75b.jpg"
+  }
+]);
