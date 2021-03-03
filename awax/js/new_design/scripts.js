07ecd147d3c99da5a6c69289417ede9a8623bebe
dp.ua <dp.ua@mail.com>
2021-03-03 10:47:19 +0200

    Add files via upload

--- awax/js/new_design/scripts.js
+++ awax/js/new_design/scripts.js
@@ -412,6 +412,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
 	const tabs=document.querySelectorAll('.info-block__item-icon'),
 	tabsContent=document.querySelectorAll('.info-block__content-block'),
 	tabsParent=document.querySelector('.info-block__item');
+	var tabId=0;
 console.log('start');
 	function hideTabsContent(){
 		tabs.forEach(i => {
@@ -425,7 +426,7 @@ console.log('start');
 		});
 	};
 	function showTabsContent(i=0){
-
+		tabId=i;
 		tabsContent[i].classList.remove('item-hide');
 		tabsContent[i].classList.add('item-show');
 		tabs[i].classList.add('active');
@@ -444,6 +445,60 @@ console.log('start');
 		};
 	});
 
+
+
+alert("swipe");
+
+var xDown = null;                                                        
+var yDown = null;                                                        
+
+function handleTouchStart(evt) {                                         
+    xDown = evt.touches[0].clientX;                                      
+    yDown = evt.touches[0].clientY;                                      
+};   
+
+function swipe(e){
+	let i=tabId+e;
+	if(i>(tabs.length-1))i=0;
+	if(i<0)i=(tabs.length-1);
+	hideTabsContent();
+	showTabsContent(i);
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+    // немного поясню здесь. Тут берутся модули движения по оси абсцисс и ординат (почему модули? потому что если движение сделано влево или вниз, то его показатель будет отрицательным) и сравнивается, чего было больше: движения по абсциссам или ординатам. Нужно это для того, чтобы, если пользователь провел вправо, но немного наискосок вниз, сработал именно коллбэк для движения вправо, а ни как-то иначе.
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+			swipe(1);
+        } else {
+            /* right swipe */
+			swipe(-1);
+        }                       
+    } else { // Это вам, в общем-то, не надо, вы ведь только влево-вправо собираетесь двигать
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+        } else { 
+            /* down swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+// Вешаем на прикосновение функцию handleTouchStart
+tabsParent.addEventListener('touchstart', handleTouchStart, false);  
+// А на движение пальцем по экрану - handleTouchMove      
+tabsParent.addEventListener('touchmove', handleTouchMove, false);
 
 
 
